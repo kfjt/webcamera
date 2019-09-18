87d5dc3998dbe2184c7ae3cc9eba96cc560b0c3e
kfjt <kfjt@users.noreply.github.com>
2019-09-18 05:34:22 +0000

    Automated deployment: Wed Sep 18 05:34:22 UTC 2019 de0d68b8fb4fcc62970a12eaef90c3b611098796

---
+++ precache-manifest.00249b2038cf1ad2b16110ac2abeb33e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4618dd684571170a3d44e24e524068",
+    "url": "/webcamera/index.html"
+  },
+  {
+    "revision": "240ac0cf3e2541719911",
+    "url": "/webcamera/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1b3b9d280d411c4e438b",
+    "url": "/webcamera/static/js/2.68070f4e.chunk.js"
+  },
+  {
+    "revision": "240ac0cf3e2541719911",
+    "url": "/webcamera/static/js/main.a2112ef4.chunk.js"
+  },
+  {
+    "revision": "2fad612e28bca00a150d",
+    "url": "/webcamera/static/js/runtime~main.346eea81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webcamera/static/media/logo.5d5d9eef.svg"
+  }
+]);
