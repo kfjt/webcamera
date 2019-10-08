17c5d486ff14b2cc8088c67759f1cc572723e72c
kfjt <k.fujita@jcom.home.ne.jp>
2019-10-09 06:35:00 +0900

    feat: drawAnonymous

--- src/App.js
+++ src/App.js
@@ -14,11 +14,29 @@ function App() {
     const drawKeypoints = (keypoints, ctx) => {
       keypoints.forEach(({score, part, position}) => {
         const {x, y} = position
-        ctx.beginPath();
-        ctx.arc(x, y, 3, 0, 2 * Math.PI);
-        ctx.fillStyle = 'red';
-        ctx.fill();
+        ctx.beginPath()
+        ctx.arc(x, y, 3, 0, 2 * Math.PI)
+        ctx.fillStyle = 'red'
+        ctx.fill()
       })
+    }
+
+    const drawAnonymous = (keypoints, ctx) => {
+      const eyesEars = keypoints.filter(({part}) => ['leftEye', 'rightEye', 'leftEar', 'rightEar'].includes(part))
+      const xs = eyesEars.map(({position}) => position.x)
+      const ys = eyesEars.map(({position}) => position.y)
+      const xmax = Math.max(...xs)
+      const xmin = Math.min(...xs)
+      const ymax = Math.max(...ys)
+      const ymin = Math.min(...ys)
+      ctx.beginPath()
+      ctx.moveTo(xmax, ymax)
+      ctx.lineTo(xmax, ymin)
+      ctx.lineTo(xmin, ymin)
+      ctx.lineTo(xmin, ymax)
+      ctx.fillStyle = 'black'
+      ctx.fill()
+      
     }
 
     const drawFrame = async () => {
@@ -34,11 +52,10 @@ function App() {
         scoreThreshold: 0.6,
         nmsRadius: 20})
 
-      pose
-        .filter(({score}) => 0.5 < score)
-        .forEach(({keypoints}) => {
-          drawKeypoints(keypoints, ctx)
-        })
+      const pose_filtered = pose.filter(({score}) => 0.5 < score)
+      
+      pose_filtered.forEach(({keypoints}) => drawKeypoints(keypoints, ctx))
+      pose_filtered.forEach(({keypoints}) => drawAnonymous(keypoints, ctx))
 
       requestAnimationFrame(drawFrame)
     }
