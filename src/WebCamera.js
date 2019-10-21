a987ae46d28a8e7b31acea56af662131da0be700
kfjt <k.fujita@jcom.home.ne.jp>
2019-10-21 11:21:45 +0900

    feat: rm rf

--- src/WebCamera.js
+++ src/WebCamera.js
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect} from 'react'
 import * as posenet from '@tensorflow-models/posenet'
-import * as tf from '@tensorflow/tfjs'
+// import * as tf from '@tensorflow/tfjs'
 
 function WebCamera() {
   const videoEl = useRef(null)
@@ -10,53 +10,53 @@ function WebCamera() {
     const video = videoEl.current
     const canvas = canvasEl.current
 
-    const drawKeypoints = (keypoints, ctx) => {
-      keypoints.forEach(({score, part, position}) => {
-        const {x, y} = position
-        ctx.beginPath()
-        ctx.arc(x, y, 3, 0, 2 * Math.PI)
-        ctx.fillStyle = 'red'
-        ctx.fill()
-      })
-    }
+    // const drawKeypoints = (keypoints, ctx) => {
+    //   keypoints.forEach(({score, part, position}) => {
+    //     const {x, y} = position
+    //     ctx.beginPath()
+    //     ctx.arc(x, y, 3, 0, 2 * Math.PI)
+    //     ctx.fillStyle = 'red'
+    //     ctx.fill()
+    //   })
+    // }
 
-    const drawAnonymous = (keypoints, ctx) => {
-      const eyesEars = keypoints.filter(({part}) => ['leftEye', 'rightEye', 'leftEar', 'rightEar'].includes(part))
-      const xs = eyesEars.map(({position}) => position.x)
-      const ys = eyesEars.map(({position}) => position.y)
-      const xmax = Math.max(...xs)
-      const xmin = Math.min(...xs)
-      const ymax = Math.max(...ys)
-      const ymin = Math.min(...ys)
-      ctx.beginPath()
-      ctx.moveTo(xmax, ymax)
-      ctx.lineTo(xmax, ymin)
-      ctx.lineTo(xmin, ymin)
-      ctx.lineTo(xmin, ymax)
-      ctx.fillStyle = 'black'
-      ctx.fill()
+    // const drawAnonymous = (keypoints, ctx) => {
+    //   const eyesEars = keypoints.filter(({part}) => ['leftEye', 'rightEye', 'leftEar', 'rightEar'].includes(part))
+    //   const xs = eyesEars.map(({position}) => position.x)
+    //   const ys = eyesEars.map(({position}) => position.y)
+    //   const xmax = Math.max(...xs)
+    //   const xmin = Math.min(...xs)
+    //   const ymax = Math.max(...ys)
+    //   const ymin = Math.min(...ys)
+    //   ctx.beginPath()
+    //   ctx.moveTo(xmax, ymax)
+    //   ctx.lineTo(xmax, ymin)
+    //   ctx.lineTo(xmin, ymin)
+    //   ctx.lineTo(xmin, ymax)
+    //   ctx.fillStyle = 'black'
+    //   ctx.fill()
       
-    }
+    // }
 
     const drawFrame = async () => {
-      const model = await posenet.load({})
+      // const model = await posenet.load({})
 
       const ctx = canvas.getContext('2d')
       ctx.drawImage(video, 0, 0)
 
-      const tensor = tf.browser.fromPixels(canvas)
-      const pose = await model.estimateMultiplePoses(tensor, {
-        flipHorizontal: false,
-        maxDetections: 5,
-        scoreThreshold: 0.6,
-        nmsRadius: 20})
-      tensor.dispose()
-      model.dispose()
+      // const tensor = tf.browser.fromPixels(canvas)
+      // const pose = await model.estimateMultiplePoses(tensor, {
+      //   flipHorizontal: false,
+      //   maxDetections: 5,
+      //   scoreThreshold: 0.6,
+      //   nmsRadius: 20})
+      // tensor.dispose()
+      // model.dispose()
 
-      const pose_filtered = pose.filter(({score}) => 0.5 < score)
+      // const pose_filtered = pose.filter(({score}) => 0.5 < score)
       
-      pose_filtered.forEach(({keypoints}) => drawKeypoints(keypoints, ctx))
-      pose_filtered.forEach(({keypoints}) => drawAnonymous(keypoints, ctx))
+      // pose_filtered.forEach(({keypoints}) => drawKeypoints(keypoints, ctx))
+      // pose_filtered.forEach(({keypoints}) => drawAnonymous(keypoints, ctx))
 
       requestAnimationFrame(drawFrame)
     }
