481607c0a2592f4dccd3fb7b76da175a57321a06
kfjt <k.fujita@jcom.home.ne.jp>
2019-10-23 12:03:09 +0900

    feat: add Geolocation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css'
 
 import WebCamera from './WebCamera'
 import VRScene from './VRScene'
+import Geolocation from './Geolocation'
 
 const App = () => {
   return (
     <div className="App">
+      <Geolocation />
       <WebCamera />
       <VRScene />
     </div>
