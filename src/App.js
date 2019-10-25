d0d1f11f569ec57ef39e1865d63b29fbbdafa1bb
kfjt <k.fujita@jcom.home.ne.jp>
2019-10-25 14:56:56 +0900

    feat: npm i react-aframe-ar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react'
 import './App.css'
 
 import WebCamera from './WebCamera'
-import VRScene from './VRScene'
+// import VRScene from './VRScene'
 import Geolocation from './Geolocation'
-// import ARScene from './ARScene'
+import ARScene from './ARScene'
 
 const App = () => {
   const relative = { position: 'relative' }
   const absolute = { position: 'absolute', top: 0, left: 0 }
   return (
     <div className="App" style={relative}>
-      <WebCamera position={absolute} />
-      {/* <ARScene position={absolute} /> */}
+      <ARScene position={absolute} />
+      {/* <WebCamera position={absolute} /> */}
       <Geolocation position={absolute} />
-      <VRScene position={absolute} />
+      {/* <VRScene position={absolute} /> */}
     </div>
   )
 }
