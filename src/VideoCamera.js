612db1c577c022dcea604f62394e43e56583ef92
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-05 14:53:17 +0900

    feat: add SelectVideo

--- src/VideoCamera.js
+++ src/VideoCamera.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react'
 
 const VideoCamera = props => {
-  const { videoEl } = props
+  const { videoEl, deviceId } = props
 
   useEffect(() => {
     const video = videoEl.current
     const setVideoStream = async () => {
       const { mediaDevices } = navigator
       if (mediaDevices && video !== null) {
-        video.srcObject = await mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+        video.srcObject = await mediaDevices.getUserMedia({ video: { deviceId, facingMode: 'environment' } })
       }
     }
     setVideoStream()
-  }, [videoEl])
+  }, [videoEl, deviceId])
 
   return <video ref={videoEl} />
 }
--- src/WebCamera.js
+++ src/WebCamera.js
@@ -3,6 +3,7 @@ import React, { useRef, useState } from 'react'
 import VideoCamera from './VideoCamera'
 import OriginalVideo from './OriginalVideo'
 import Keypoints from './Keypoints'
+import SelectVideo from './SelectVideo'
 
 const WebCamera = props => {
   const { position } = props
@@ -11,14 +12,11 @@ const WebCamera = props => {
   const relative = { position: 'relative' }
   const absolute = { position: 'absolute', top: 0, left: 0 }
 
-  if (!deviceId) {
-    return (<div>select device</div>)
-  }
-  setDeviceId(1)
+  if (!deviceId) return (<SelectVideo setDeviceId={setDeviceId} />)
 
   return (
     <div className="WebCamera" style={position}>
-      <VideoCamera className="Input" videoEl={videoEl} />
+      <VideoCamera className="Input" videoEl={videoEl} deviceId={deviceId} />
       <div className="Output" style={relative} >
         <OriginalVideo className="layer" style={absolute} videoEl={videoEl} />
         <Keypoints className="layer" style={absolute} videoEl={videoEl} />
