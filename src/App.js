9d1a88a7d91cc1ed3e60ef46731563afef55d347
kfjt <k.fujita@jcom.home.ne.jp>
2019-11-07 15:56:59 +0900

    feat: DepthView

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import './App.css'
 
 import Realsense from './Realsense'
+import DepthView from './DepthView'
 // import WebCamera from './WebCamera'
 // import VRScene from './VRScene'
 // import Geolocation from './Geolocation'
@@ -15,6 +16,7 @@ const App = () => {
   return (
     <div className="App">
       <Realsense videoRGB={videoRGB} videoDepth={videoDepth} />
+      <DepthView videoDepth={videoDepth} />
     </div>
   )
 }
---
+++ src/DepthView.js
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react'
+
+const DepthView = props => {
+  const { videoDepth } = props
+  const depthView = useRef()
+  const frame = useRef(0)
+
+  useEffect(() => {
+    const video = videoDepth.current
+    const canvas = depthView.current
+
+    // const invert = data => {
+    //   for (let i = 0; i < data.length; i += 4) {
+    //     data[i] = 255 - data[i]         // red
+    //     data[i + 1] = 255 - data[i + 1] // green
+    //     data[i + 2] = 255 - data[i + 2] // blue
+    //   }
+    // }
+    const color = data => {
+      for (let index = 0; index < data.length; index += 4) {
+        data[index] *= 240
+        data[index + 1] *= 240
+        data[index + 2] *= 240
+      }
+    }
+
+    const drawFrame = () => {
+      if (60 < frame.current) {
+        video.pause()
+      }
+      frame.current += 1
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(video, 0, 0)
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+      color(imageData.data)
+      ctx.putImageData(imageData, 0, 0)
+      requestAnimationFrame(drawFrame)
+    }
+
+    video.addEventListener('loadedmetadata', () => {
+      canvas.width = video.videoWidth
+      canvas.height = video.videoHeight
+    })
+    video.addEventListener('loadeddata', drawFrame)
+  }, [videoDepth, frame])
+
+
+  return (<canvas className="DepthView" ref={depthView} />)
+}
+
+export default DepthView
